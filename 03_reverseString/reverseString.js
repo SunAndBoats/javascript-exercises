2350dad7026ff42bb6d1f978b192e585ca5ec757
SunAndBoats <201173636+SunAndBoats@users.noreply.github.com>
2025-03-18 12:50:35 +1300

    reverseString.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,19 @@
-const reverseString = function() {
+const reverseString = function(input) {
+    //convertimos string into array
+    const reversed=input.split("").reverse().join("")
+    return reversed
+        
+
+   
 
 };
+
+
+
+
+
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
