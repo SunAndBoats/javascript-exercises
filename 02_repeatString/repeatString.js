f77c3fd91f3d379a021b26a174916634932696ec
SunAndBoats <201173636+SunAndBoats@users.noreply.github.com>
2025-03-18 11:56:37 +1300

    repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,28 @@
-const repeatString = function() {
+const repeatString = function(word, times) {
+if(times<0) return "ERROR"
+//create variable to return
+let string=""
 
+//solucion 2 transformamos el string en un array y le añadimos 1 por cada iteracion de un bucle for
+for(let i=0;i<times;i++){
+    string+=word
+};
+return string;
 };
 
+
 // Do not edit below this line
-module.exports = repeatString;
+module.exports = repeatString; 
+ /*
+
+const repeatString = function (word, times) {
+    if (times < 0) return "ERROR";
+    let string = "";
+    for (let i = 0; i < times; i++) {
+      string += word;
+    }
+    return string;
+  };
+  
+  module.exports = repeatString;
+*/
