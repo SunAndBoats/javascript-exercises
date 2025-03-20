e6074b674d6a5193fc07e2f381df4c7d8a72ce0b
SunAndBoats <201173636+SunAndBoats@users.noreply.github.com>
2025-03-20 19:51:08 +1300

    calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,36 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  return array.reduce((total, num) => total + num, 0);
 };
 
-const multiply = function() {
 
+
+const multiply = function (array) {
+  return array.reduce((total, num) => total * num, 1);
+};
+  
+
+
+const power = function(a,b) {
+	return a**b
 };
 
-const power = function() {
-	
+const factorial = function (n) {
+  if (n === 0) return 1;
+  let result = 1;
+  for (let i = n; i > 1; i--) {
+    result *= i;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
-};
 
 // Do not edit below this line
 module.exports = {
